5adfeb2d7df755b22520ee0240f8a2505262cbbf
Aleksej <git@aleksejdix.com>
2020-06-01 14:31:00 +0200

    lesson 3

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <div>
-    <AppButton @click="log">
-      <AppIcon name="heart" />
-      add to favorites
-    </AppButton>
-  </div>
+  <AppLayout>
+    <template #header>Slots are great</template>
+    <template #default>
+      <AppButton @click="log">
+        <AppIcon name="heart" />
+        add to favorites
+      </AppButton>
+    </template>
+    <template #footer>made with slots</template>
+  </AppLayout>
 </template>
 
 <script>
+import AppLayout from "@/components/AppLayout";
 import AppButton from "@/components/AppButton";
 import AppIcon from "@/components/AppIcon";
 
 export default {
   components: {
+    AppLayout,
     AppButton,
     AppIcon
   },
---
+++ src/components/AppLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="layout">
+    <header>
+      <slot name="header" />
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <slot name="footer" />
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.layout {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.layout header,
+.layout footer {
+  background: #41b883;
+  color: white;
+  padding: 1rem;
+  flex: 0 0 auto;
+}
+
+.layout main {
+  flex: 1 1 auto;
+}
+</style>
