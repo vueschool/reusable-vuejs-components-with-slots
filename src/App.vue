e60389ada018eab6f376bca22d5cf3ebfb3c755c
Aleksej <git@aleksejdix.com>
2020-06-01 14:16:47 +0200

    lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <AppButton>press me</AppButton>
+    <AppButton>like me</AppButton>
+    <AppButton>buy me</AppButton>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppButton from "@/components/AppButton";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AppButton
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AppButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button><slot></slot></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style scoped>
+button {
+  color: white;
+  display: flex;
+  align-items: center;
+  padding: 0.25rem 0.5rem;
+  border: none;
+  text-transform: uppercase;
+  font-weight: bold;
+  background-color: #41b883;
+  border-radius: 0.25rem;
+  line-height: 1.5;
+  margin: 10px;
+}
+</style>
