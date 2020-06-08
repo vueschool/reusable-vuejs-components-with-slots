06412b874fda6816afc92a68a856f782ea86b3bf
Aleksej <git@aleksejdix.com>
2020-06-08 18:56:58 +0200

    lesson 9

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,56 @@
 <template>
-  <AppUserList>
-    <template #userlist="{list, count, remove}">
-      <AppUserCardsList :list="list">
-        <template #secondrow="{item}">
-          <AppButton @click="remove(item)">remove</AppButton>
-        </template>
-      </AppUserCardsList>
-    </template>
-  </AppUserList>
+  <div class="page">
+
+    <select v-model="selected">
+      <option 
+        v-for="option in options" 
+        :value="option.value" 
+        :key="option.value">
+        {{option.label}}
+      </option>
+    </select>
+
+
+    <AppUserList>
+      <template #userlist="{list}">
+        <AppUserCardsList :list="list">
+          <template #[selected]="{text}">
+            <h4>{{text}}</h4>
+          </template>
+        </AppUserCardsList>
+      </template>
+    </AppUserList>
+  </div>
 </template>
 
 <script>
-import AppButton from "@/components/AppButton";
 import AppUserList from "@/components/AppUserList";
 import AppUserCardsList from "@/components/AppUserCardsList";
 export default {
   components: {
-    AppButton,
     AppUserList,
     AppUserCardsList
+  },
+  data() {
+    return {
+      selected: 'first',
+      options: [
+        { value: 'first',  label: 'first name'},
+        { value: 'last',  label: 'last name'},
+        { value: 'full',  label: 'fullname'},
+        { value: 'fullWithTitle',  label: 'fullname with title'},
+      ]
+    }
   }
 };
 </script>
+
+<style>
+.page {
+  padding: 2rem;
+}
+
+.page > * + * {
+  margin-top: 2rem;
+}
+</style>
